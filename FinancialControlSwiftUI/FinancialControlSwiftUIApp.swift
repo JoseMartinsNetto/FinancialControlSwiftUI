b0be96a3a48c99d5f0f8553dac7b58c53ce73e2c
José Martins <j.msantos.netto@gmail.com>
2023-03-01 20:32:52 -0300

    Initial Commit

---
+++ FinancialControlSwiftUI/FinancialControlSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialControlSwiftUIApp.swift
+//  FinancialControlSwiftUI
+//
+//  Created by Jose Martins on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancialControlSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
